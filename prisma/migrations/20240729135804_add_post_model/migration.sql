c50c9511d6432aeadf2e9a7f13d710320e19e19e
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-07-29 18:09:18 +0200

    :card_file_box: add User and Posts Models

--- prisma/migrations/20240729135804_add_post_model/migration.sql
+++ prisma/migrations/20240729135804_add_post_model/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE `Posts` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
-    `isLiked` BOOLEAN NOT NULL DEFAULT false,
+    `description` VARCHAR(191) NOT NULL,
+    `content` VARCHAR(191) NOT NULL,
     `isDraft` BOOLEAN NOT NULL DEFAULT false,
-    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `updatedAt` DATETIME(3) NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
