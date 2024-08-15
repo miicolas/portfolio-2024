2a90ede404eb1958ab0bcc13516c83c6929e8407
Nicolas Becharat <nicolas.becharat@gmail.com>
2024-08-15 22:20:19 +0200

    add ui ux

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,34 @@
+import Header from "@/components/layout/header";
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export default async function Post({ params }: { params: { slug: string } }) {
-    const { slug } = params;
-    const post = await prisma.posts.findFirst({
-        where: {
-            slug,
-        }
-    })
-   
+  const { slug } = params;
+  const post = await prisma.posts.findFirst({
+    where: {
+      slug,
+    },
+  });
 
-    return (
-        <div>
-            <h1>{post?.title}</h1>
-            
-            <div dangerouslySetInnerHTML={{ __html: post?.content || "" }} />
-            <p>{post?.date}</p>
-            <p>{post?.isDraft}</p>
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <main className="py-5">
+        <Button asChild>
+          <Link href="/blog" className="flex items-center gap-2">
+          
+          <ArrowLeft />
+          Back to Blog</Link>
+        </Button>
+        <section className="py-10">
+          <h1>{post?.title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: post?.content || "" }} />
+          <p>{post?.date}</p>
+          <p>{post?.isDraft}</p>
+        </section>
+      </main>
+    </>
+  );
 }
